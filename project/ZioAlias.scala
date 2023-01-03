c04f3c0cd26b65474cdb4844ed91c00271aedc68
Scala Steward <scala_steward@virtuslab.com>
2023-01-03 13:34:23 +0000

    Update async-http-client-backend-zio, ... to 3.8.6

--- project/ZioAlias.scala
+++ project/ZioAlias.scala
@@ -6,22 +6,25 @@ sealed abstract class ZioAlias(
     val interop: ModuleID,
     val sttp: ModuleID,
     val tapir: ModuleID,
-) extends Product with Serializable
+) extends Product
+    with Serializable
 object ZioAlias {
-  def interop(version: String) = "dev.zio" %% "zio-interop-cats" % version
-  def sttp(name: String) = "com.softwaremill.sttp.client3" %% s"async-http-client-backend-$name" % "3.6.2"
-  def tapir(name: String) = "com.softwaremill.sttp.tapir" %% s"tapir-$name-http-server" % "1.0.0"
+  def interop(version: String) = "dev.zio"                       %% "zio-interop-cats"                 % version
+  def sttp(name: String)       = "com.softwaremill.sttp.client3" %% s"async-http-client-backend-$name" % "3.8.6"
+  def tapir(name: String)      = "com.softwaremill.sttp.tapir"   %% s"tapir-$name-http-server"         % "1.0.0"
 
-  case object zio1 extends ZioAlias(
-    "1.0.15",
-    interop("3.2.9.1"),
-    sttp("zio1"),
-    tapir("zio1")
-  )
-  case object zio2 extends ZioAlias(
-    "2.0.0-RC6",
-    interop("3.3.0-RC7"),
-    sttp("zio"),
-    tapir("zio")
-  )
+  case object zio1
+      extends ZioAlias(
+        "1.0.15",
+        interop("3.2.9.1"),
+        sttp("zio1"),
+        tapir("zio1"),
+      )
+  case object zio2
+      extends ZioAlias(
+        "2.0.0-RC6",
+        interop("3.3.0-RC7"),
+        sttp("zio"),
+        tapir("zio"),
+      )
 }
